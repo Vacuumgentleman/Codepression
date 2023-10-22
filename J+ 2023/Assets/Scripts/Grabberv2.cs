9b103b11f870479af7a4a48d01c6c99d4ebcff29
Vacuumgentleman <Thevacuumgentleman@gmail.com>
2023-10-22 11:04:34 -0500

    Bloqueo fichas
    las fichas se bloquean tras ingresar a la pos correcta

--- J+ 2023/Assets/Scripts/Grabberv2.cs
+++ J+ 2023/Assets/Scripts/Grabberv2.cs
@@ -2,34 +2,30 @@ using UnityEngine;
 
 public class Grabberv2 : MonoBehaviour
 {
-    // Variables para el objeto seleccionado y su estado de arrastre
-    private GameObject selectedObject;
-    private Vector3 originalPosition;
-    private Quaternion originalRotation;
-    private bool isDragging;
+    private GameObject selectedObject; // El objeto seleccionado actualmente.
+    private Vector3 originalPosition; // La posición original del objeto antes de arrastrarlo.
+    private Quaternion originalRotation; // La rotación original del objeto antes de arrastrarlo.
+    private bool isDragging; // Indica si se está arrastrando un objeto.
 
-    // Sonidos y distancia máxima para soltar
-    [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip pickupClip;
-    [SerializeField] private AudioClip dropClip;
+    [SerializeField] private AudioSource audioSource; // Referencia al componente AudioSource para reproducir sonidos.
+    [SerializeField] private AudioClip pickupClip; // Sonido de recogida.
+    [SerializeField] private AudioClip dropClip; // Sonido de soltar.
     [SerializeField] private float dropDistanceThreshold = 0.5f; // Define la distancia máxima permitida para soltar.
 
-    // Método Update se llama en cada fotograma
     private void Update()
     {
-        // Lógica para objetos arrastrados
         if (isDragging)
         {
+            // Actualiza la posición del objeto seleccionado según la posición del ratón.
+            // También permite rotar el objeto con el botón derecho del ratón.
             Vector3 mousePosition = Input.mousePosition;
-
-            // Convierte la posición del ratón en coordenadas del mundo
             mousePosition.z = Camera.main.WorldToScreenPoint(selectedObject.transform.position).z;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
             selectedObject.transform.position = new Vector3(worldPosition.x, 0f, worldPosition.z);
 
-            // Giro de objeto con clic derecho
             if (Input.GetMouseButtonDown(1))
             {
+                // Rota el objeto seleccionado en 45 grados alrededor del eje Y.
                 selectedObject.transform.rotation = Quaternion.Euler(new Vector3(
                     selectedObject.transform.rotation.eulerAngles.x,
                     selectedObject.transform.rotation.eulerAngles.y + 45f,
@@ -37,66 +33,77 @@ public class Grabberv2 : MonoBehaviour
             }
         }
 
-        // Lógica para seleccionar y soltar objetos con clic izquierdo
         if (Input.GetMouseButtonDown(0))
         {
-            // Si no hay un objeto seleccionado
             if (selectedObject == null)
             {
-                // Lanza un rayo para determinar si se hizo clic en un objeto
+                // Lanza un rayo para determinar qué objeto se ha hecho clic.
                 RaycastHit hit = CastRay();
-
-                // Si se golpea un objeto válido, lo selecciona
                 if (hit.collider != null)
                 {
                     selectedObject = hit.collider.gameObject;
 
-                    // Comprueba si el objeto es "drag," no "slot"
+                    // Solo permite arrastrar fichas "drag," no "slot."
                     if (!selectedObject.name.StartsWith("polySurface"))
                     {
                         selectedObject = null;
                         return;
                     }
 
-                    // Oculta el cursor, guarda la posición y rotación originales y reproduce un sonido
                     Cursor.visible = false;
                     originalPosition = selectedObject.transform.position;
                     originalRotation = selectedObject.transform.rotation;
+
                     isDragging = true;
+
+                    // Reproduce el sonido de recogida.
                     audioSource.PlayOneShot(pickupClip);
                 }
             }
-            else // Si ya hay un objeto seleccionado
+            else
             {
-                // Comprueba si el objeto "drag" se corresponde con un "slot" por nombre
+                // Comprueba si el objeto "drag" se corresponde con un "slot" por nombre.
                 if (selectedObject.name.StartsWith("polySurface"))
                 {
-                    // Extrae el número del objeto y encuentra el "slot" correspondiente
                     string[] splitName = selectedObject.name.Split('e');
                     int dragNumber = int.Parse(splitName[1]);
                     string slotName = "Surface" + dragNumber;
-                    Transform slot = GameObject.Find(slotName).transform;
 
-                    // Verifica si la rotación es adecuada y si la distancia al "slot" es menor o igual a la distancia máxima permitida
-                    if (IsRotationValid(selectedObject, slot))
+                    Transform slot = GameObject.Find(slotName).transform;
+                    if (slot != null)
                     {
-                        float distanceToSlot = Vector3.Distance(selectedObject.transform.position, slot.position);
-                        if (distanceToSlot <= dropDistanceThreshold)
+                        // Verifica si la rotación es adecuada antes de soltar la ficha.
+                        if (IsRotationValid(selectedObject, slot))
                         {
-                            selectedObject.transform.position = slot.position;
-                            isDragging = false;
-                            selectedObject = null;
-                            Cursor.visible = true;
-                            audioSource.PlayOneShot(dropClip);
-                        }
-                        else
-                        {
-                            // Devuelve el objeto a su posición y rotación originales
-                            selectedObject.transform.position = originalPosition;
-                            selectedObject.transform.rotation = originalRotation;
-                            selectedObject = null;
-                            Cursor.visible = true;
-                            isDragging = false;
+                            // Verifica si la ficha está lo suficientemente cerca del "slot" antes de soltarla.
+                            float distanceToSlot = Vector3.Distance(selectedObject.transform.position, slot.position);
+                            if (distanceToSlot <= dropDistanceThreshold)
+                            {
+                                // Ajusta la posición del objeto al "slot" antes de bloquearlo.
+                                selectedObject.transform.position = slot.position;
+
+                                // Desactiva los Colliders del objeto para que no pueda interactuarse más.
+                                Collider[] colliders = selectedObject.GetComponentsInChildren<Collider>();
+                                foreach (Collider collider in colliders)
+                                {
+                                    collider.enabled = false;
+                                }
+
+                                isDragging = false;
+                                selectedObject = null;
+                                Cursor.visible = true;
+
+                                // Reproduce el sonido de soltar.
+                                audioSource.PlayOneShot(dropClip);
+                            }
+                            else
+                            {
+                                selectedObject.transform.position = originalPosition;
+                                selectedObject.transform.rotation = originalRotation;
+                                selectedObject = null;
+                                Cursor.visible = true;
+                                isDragging = false;
+                            }
                         }
                     }
                 }
@@ -104,9 +111,10 @@ public class Grabberv2 : MonoBehaviour
         }
     }
 
-    // Lanza un rayo desde la posición del ratón en la pantalla y devuelve el objeto golpeado
     private RaycastHit CastRay()
     {
+        // Lanza un rayo desde la posición del ratón para determinar qué objeto se ha hecho clic.
+        // Devuelve la información sobre el objeto impactado por el rayo.
         Vector3 screenMousePosFar = new Vector3(
             Input.mousePosition.x,
             Input.mousePosition.y,
@@ -119,23 +127,18 @@ public class Grabberv2 : MonoBehaviour
         Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
         RaycastHit hit;
         Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
+
         return hit;
     }
 
-    // Verifica si la rotación del objeto es adecuada antes de soltarlo en el "slot"
     private bool IsRotationValid(GameObject dragObject, Transform slot)
     {
-        // Agrega aquí la lógica para verificar si la rotación es adecuada.
-        // Puedes comparar la rotación actual del objeto "drag" con la rotación del "slot."
-
-        // Por ejemplo, puedes usar un ángulo máximo permitido:
+        // Verifica si la rotación actual del objeto "drag" es adecuada en relación con la rotación del "slot."
+        // Puede utilizar una lógica personalizada para determinar si la rotación es válida, por ejemplo, un ángulo máximo permitido.
         float maxRotationDifference = 10f; // Define el ángulo máximo permitido.
         Quaternion dragRotation = dragObject.transform.rotation;
         Quaternion slotRotation = slot.rotation;
-
-        // Calcula la diferencia de rotación entre el objeto "drag" y el "slot."
         float rotationDifference = Quaternion.Angle(dragRotation, slotRotation);
-
         // Comprueba si la diferencia de rotación es menor o igual que el ángulo máximo permitido.
         return rotationDifference <= maxRotationDifference;
     }
